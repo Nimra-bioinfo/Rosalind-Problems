f406b0a2e1e51f0dbeb8c6629fb78919a408bf56
Nimra-bioinfo <nimrakhan20077@gmail.com>
2026-04-19 04:32:21 -1200

    01_dna_count.py

--- 01_dna_count.py
+++ 01_dna_count.py
@@ -1,3 +1,6 @@
+# Rosalind Problem: Counting DNA Nucleotides
+# Learning: I learned how to use the .count() method in Python to find the frequency of specific characters.
+# I also practiced using the input() function to make the script interactive for any DNA sequence.
 dna = input("Enter DNA sequence: ")
 a = dna.count('A')
 c = dna.count('C')
